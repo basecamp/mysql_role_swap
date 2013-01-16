bb9660b504757d7bf553d49d5b3d92e5e47d3dcf
Jean-Francois Theroux <failshell@gmail.com>
2013-01-16 15:27:34 -0500

    removed another hardcoded slave username...

--- mysql_role_swap.rb
+++ mysql_role_swap.rb
@@ -613,7 +613,7 @@ class MysqlSwitchRoleContext
   end
 
   def demote_old_master_to_slave
-    change_master_command = "CHANGE MASTER TO MASTER_HOST='#{@slave.config['host']}', MASTER_PORT=#{@slave.config['port']}, MASTER_USER='slave', MASTER_PASSWORD='#{@slave.config['slave_password']}',MASTER_LOG_FILE='#{@slave_binlog_file}', MASTER_LOG_POS=#{@slave_binlog_position}"
+    change_master_command = "CHANGE MASTER TO MASTER_HOST='#{@slave.config['host']}', MASTER_PORT=#{@slave.config['port']}, MASTER_USER='#{@slave.config['username']}', MASTER_PASSWORD='#{@slave.config['slave_password']}',MASTER_LOG_FILE='#{@slave_binlog_file}', MASTER_LOG_POS=#{@slave_binlog_position}"
     if @master
       @master.connection.execute(change_master_command)
       @master.connection.execute("START SLAVE")
